10469caa7e6993528ef8bfe1c4c269bd18882af0
Jean Gomes <neutrinomuon@gmail.com>
2024-01-24 19:07:15 +0000

    Update entrypoint.sh

--- .github/actions/update-readme/entrypoint.sh
+++ .github/actions/update-readme/entrypoint.sh
@@ -4,7 +4,7 @@ echo "================================================================="
 
 # Setup Git interface
 # Track who is using this action to update the README.md file
-git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.name "${INPUT_NAME}"
 git config --global user.email "${INPUT_EMAIL}"
 # Make sure to add to the safe list of directories
 git config --global --add safe.directory /github/workspace
